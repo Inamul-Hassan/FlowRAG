2c10959c38c194c13df1a46271ddc3c84be515eb
Inamul Hassan <hassan.inamul@outlook.com>
2024-02-25 23:38:22 +0530

    Setup Project
    + Created project directories
    + Added .gitignore
    + Added piplock
    + Init general config json

---
+++ backend/load.py
@@ -0,0 +1,47 @@
+from llama_index.core import SimpleDirectoryReader
+
+SUPPORTED_EXTENSIONS = [".hwp", ".pdf", ".docx", ".pptx", ".ppt", ".pptm", ".jpg",
+                        ".png", ".jpeg", ".mp3", ".mp4", ".csv", ".epub", ".md", ".mbox", ".ipynb", ".txt", ".json"]
+
+# validating user input
+LOADER_EXTENSTIONS_MAPPING = {
+    "SimpleDirectoryReader": [".hwp", ".pdf", ".docx", ".pptx", ".ppt", ".pptm", ".jpg",
+                              ".png", ".jpeg", ".mp3", ".mp4", ".csv", ".epub", ".md", ".mbox", ".ipynb", ".txt", ".json"]
+}
+
+
+def validate_input_file(input_file: str) -> str:
+    extenstion = input_file.split(".")[-1]
+    pass
+
+
+# Load
+"""
+        ".hwp": HWPReader,
+        ".pdf": PDFReader,
+        ".docx": DocxReader,
+        ".pptx": PptxReader,
+        ".ppt": PptxReader,
+        ".pptm": PptxReader,
+        ".jpg": ImageReader,
+        ".png": ImageReader,
+        ".jpeg": ImageReader,
+        ".mp3": VideoAudioReader,
+        ".mp4": VideoAudioReader,
+        ".csv": PandasCSVReader,
+        ".epub": EpubReader,
+        ".md": MarkdownReader,
+        ".mbox": MboxReader,
+        ".ipynb": IPYNBReader,
+        ".txt": TextReader,
+"""
+
+
+reader = SimpleDirectoryReader(
+    input_dir="storage/data",
+)
+
+docs = reader.load_data()
+print(f"Loaded {len(docs)} docs")
+
+print(type(docs[0]))

c35ea07fbe7288ed3d0eedfeb9367112eb3a59f8
vishal.sr@hotmail.com <vishal.sr@hotmail.com>
2024-02-29 00:19:10 +0530

    Add more LLMs

--- backend/models/llm.py
+++ backend/models/llm.py
@@ -1,5 +1,25 @@
 from llama_index.llms.gemini import Gemini
+from llama_index.llms.openai import OpenAI
+from llama_index.llms.ollama import Ollama
+from llama_index.llms.anthropic import Anthropic
+from llama_index.llms.mistralai import MistralAI
 
 def chat_using_Gemini(config: dict = {"model_name": "models/gemini-pro"}) -> Gemini:
     llm = Gemini(**config)
+    return llm
+
+def chat_using_OpenAI(config: dict = {"model": "gpt-3.5-turbo"}) -> OpenAI:
+    llm = OpenAI(**config)
+    return llm
+
+def chat_using_Ollama(config: dict = {"model": "llama-2"}) -> Ollama:
+    llm = Ollama(**config)
+    return llm
+
+def chat_using_Anthropic(config: dict = {"model": "claude-2"}) -> Anthropic:
+    llm = Anthropic(**config)
+    return llm
+
+def chat_using_MistralAI(config: dict = {"model": "mistral-tiny"}) -> MistralAI:
+    llm = MistralAI(**config)
     return llm

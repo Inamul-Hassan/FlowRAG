99537f4d96397196fc95a724785e3802d2a4e68b
Inamul Hassan <hassan.inamul@outlook.com>
2024-02-29 21:36:05 +0530

    Add DB support for chroma and qdrant
    + Implemented ChromaDB and Qdranr
    + Updated options

--- backend/index.py
+++ backend/index.py
@@ -1,19 +1,47 @@
 from llama_index.core import VectorStoreIndex, SummaryIndex
-from typing import Sequence
+from typing import Sequence, Optional
 from llama_index.core.schema import BaseNode
-from llama_index.core.readers import Document
+# from llama_index.core.readers import Document
+from llama_index.core import StorageContext
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.vector_stores.qdrant import QdrantVectorStore
+import chromadb
+from qdrant_client import QdrantClient
 
 import os
+from typing import Dict
 from dotenv import load_dotenv
 load_dotenv()
 
-def get_vector_index_using_VectorStoreIndex(nodes: Sequence[BaseNode],embedding_model) -> VectorStoreIndex:
-    vector_index = VectorStoreIndex(nodes=nodes,embed_model=embedding_model)
+
+def save_to_chromadb(collection_name:str)->StorageContext:
+    db = chromadb.PersistentClient(path="storage/chromadb")
+    chroma_collection = db.get_or_create_collection(name=collection_name)
+    
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    return storage_context
+    
+def save_to_qdrant(collection_name:str,isLocal:bool=True,config:Optional[Dict]=None)->StorageContext:
+    if isLocal:
+        client = QdrantClient(location=":memory:",path="storage/qdrant")
+    else:
+        client = QdrantClient(**config)
+    vector_store = QdrantVectorStore(client=client, collection_name=collection_name)
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    return storage_context
+
+def get_index_using_VectorStoreIndex(nodes: Sequence[BaseNode],embedding_model,storage_context:StorageContext=None) -> VectorStoreIndex:
+    vector_index = VectorStoreIndex(nodes=nodes,embed_model=embedding_model,storage_context=storage_context)
     return vector_index
 
-def get_summary_index_using_SummaryIndex(nodes: Sequence[BaseNode]) -> SummaryIndex:
-    summary_index = SummaryIndex(nodes=nodes)
+def get_index_using_SummaryIndex(nodes: Sequence[BaseNode],storage_context:StorageContext=None) -> SummaryIndex:
+    summary_index = SummaryIndex(nodes=nodes,storage_context=storage_context)
     return summary_index
+
+# def store_locally(index):
+#     index.
+
 
 if __name__ == "__main__":
     pass
--- testing/test_pipeline_01.py
+++ testing/test_pipeline_01.py
@@ -1,22 +1,29 @@
 import sys
-sys.path.append(r'C:\Users\vishal\Documents\AI\RAG pipeline\FlowRAG')
+# sys.path.append(r'C:\Users\vishal\Documents\AI\RAG pipeline\FlowRAG')
+sys.path.append("E:\Python\Directory\FlowRAG-main")
 
 print(sys.path)
 
 from backend import load
 from backend import transform
 from backend import index
+from backend.models import embedding
 
 from llama_index.llms.gemini import Gemini
 
 
+
 documents = load.get_docs_using_SimpleDirectoryReader("storage/data")
-nodes = transform.get_nodes_using_recursive_character_text_splitter(
+nodes = transform.transform_using_RecursiveCharacterTextSplitter(
     documents, {"chunk_size":  4000,
                 "chunk_overlap": 200,
                 "separators": ["\n\n", "\n", " ", ""]})
-embedding_model = index.get_embedding_using_GeminiEmbedding()
+embedding_model = embedding.embed_using_GeminiEmbedding(model_name="models/embedding-001")
 vector_index = index.get_vector_index_using_VectorStoreIndex(nodes=nodes,embedding_model=embedding_model)
+
+vector_index.storage_context.persist("storage/db")
+
+
 
 llm = Gemini(model_name="models/gemini-pro")
 

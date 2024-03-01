457d781d8739aaf2f291637f27843ba9e9829039
Inamul Hassan <hassan.inamul@outlook.com>
2024-03-01 07:07:42 +0530

    Refactor testing files
    + Modified test01 and test02

--- testing/test01_pipeline.py
+++ testing/test01_pipeline.py
@@ -2,36 +2,34 @@ import sys
 # sys.path.append(r'C:\Users\vishal\Documents\AI\RAG pipeline\FlowRAG')
 sys.path.append("E:\Python\Directory\FlowRAG-main")
 
-print(sys.path)
-
 from backend import load
 from backend import transform
 from backend import index
 from backend.models import embedding
-
 from llama_index.llms.gemini import Gemini
 
+# Load the models
+embedding_model = embedding.embed_using_GeminiEmbedding(model_name="models/embedding-001")
+llm = Gemini(model_name="models/gemini-pro")
 
-
+# Load the documents
 documents = load.get_docs_using_SimpleDirectoryReader("storage/data")
+
+# Transform the documents
 nodes = transform.transform_using_RecursiveCharacterTextSplitter(
     documents, {"chunk_size":  4000,
                 "chunk_overlap": 200,
                 "separators": ["\n\n", "\n", " ", ""]})
-embedding_model = embedding.embed_using_GeminiEmbedding(model_name="models/embedding-001")
+
+# Index the documents
 vector_index = index.get_index_using_VectorStoreIndex(nodes=nodes,embedding_model=embedding_model)
 
+# Persist the index
 vector_index.storage_context.persist("storage/db")
 
-
-
-llm = Gemini(model_name="models/gemini-pro")
-
+# Query the index
 retriever = vector_index.as_retriever()
 query_engine = vector_index.as_query_engine(llm=llm)
-
-
-# response = retriever.retrieve("did pual gram ever paint?")
 retriever_response =retriever.retrieve("what is vieweb?")
 query_response =  query_engine.query("what is vieweb?")
 
--- testing/test02_SimpleQueryEngine.py
+++ testing/test02_SimpleQueryEngine.py
@@ -1,43 +1,40 @@
 import sys
-sys.path.append(r'C:\Users\vishal\Documents\AI\RAG pipeline\FlowRAG')
-# sys.path.append("E:\Python\Directory\FlowRAG-main")
-
-print(sys.path)
+# sys.path.append(r'C:\Users\vishal\Documents\AI\RAG pipeline\FlowRAG')
+sys.path.append("E:\Python\Directory\FlowRAG-main")
 
 from backend import load
 from backend import transform
 from backend import index
 from backend.models import embedding
-
 from llama_index.llms.gemini import Gemini
 from backend.query_engine import query_using_RetrieverQueryEngine
+from llama_index.core import Settings
 
 from dotenv import load_dotenv
 load_dotenv()
 
+# Load the models
+embedding_model = embedding.embed_using_GeminiEmbedding(model_name="models/embedding-001")
+llm = Gemini(model_name="models/gemini-pro")
+Settings.llm = Gemini(model_name="models/gemini-pro")
+
+# Load the documents
 documents = load.get_docs_using_SimpleDirectoryReader("storage/data")
+
+# Transform the documents
 nodes = transform.transform_using_RecursiveCharacterTextSplitter(
     documents, {"chunk_size":  4000,
                 "chunk_overlap": 200,
                 "separators": ["\n\n", "\n", " ", ""]})
-embedding_model = embedding.embed_using_GeminiEmbedding(model_name="models/embedding-001")
+
+# Index the documents
 vector_index = index.get_index_using_VectorStoreIndex(nodes=nodes,embedding_model=embedding_model)
 
+# Persist the index
 # vector_index.storage_context.persist("storage/db")
 
-
-from llama_index.core import Settings
-
-Settings.llm = Gemini(model_name="models/gemini-pro")
-llm = Gemini(model_name="models/gemini-pro")
-
-# retriever = vector_index.as_retriever()
-# query_engine = vector_index.as_query_engine(llm=llm)
-
-
-# # response = retriever.retrieve("did pual gram ever paint?")
-# retriever_response =retriever.retrieve("what is vieweb?")
-query_engine = query_using_RetrieverQueryEngine(index = vector_index, llm = llm, config = {"similarity_top_k": 3, "response_mode": "compact"})
+# Query the index
+query_engine = query_using_RetrieverQueryEngine(index = vector_index, llm = llm, config = {"similarity_top_k": 3, "response_mode": "refine"})
 query_response =  query_engine.query("what is vieweb?")
 
 
